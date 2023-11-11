58d51f9486a4999e74637d6b05d73d471acbb188
Laura Rocha <laura_rocha_mx@hotmail.com>
2023-11-11 17:15:16 -0600

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
-import { HeroesModule } from './heroes/heroes.module';
+
 
 
 
@@ -15,6 +18,7 @@ import { HeroesModule } from './heroes/heroes.module';
     BrowserModule,
     CounterModule,
     HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
